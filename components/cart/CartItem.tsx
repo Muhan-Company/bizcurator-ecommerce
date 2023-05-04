ad69d4f8f35bed8a65e9f6fd0668b7d44e206d68
Hwa-J <hj1103.code@gmail.com>
2023-05-04 23:29:49 +0900

    style: CartItem 반응형 스타일 적용 - Issue #5

--- components/cart/CartItem.tsx
+++ components/cart/CartItem.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import React, { useState } from 'react';
-import { CloseIcon, DisabledMinusIcon, MinusIcon, PlusIcon, SmallCheckBoxIcon, SmallCheckedBoxIcon } from '../Icons';
+import { CheckBoxIcon, CheckedBoxIcon, CloseIcon, DisabledMinusIcon, MinusIcon, PlusIcon } from '../Icons';
 
 type CartItemPropsType = {
   isSelected: boolean;
@@ -8,19 +8,27 @@ type CartItemPropsType = {
 };
 export default function CartItem({ isSelected, setIsSelected }: CartItemPropsType) {
   return (
-    <div className="flex py-[22px] border-b-[1px] border-b-gray_02">
-      <div className="pr-3" onClick={() => setIsSelected(!isSelected)}>
-        {isSelected ? <SmallCheckedBoxIcon /> : <SmallCheckBoxIcon />}
+    <div className="flex md:items-center md:w-[800px] py-[22px] border-b-[1px] border-b-gray_02">
+      <div className="pr-3 md:pl-7" onClick={() => setIsSelected(!isSelected)}>
+        {isSelected ? <CheckedBoxIcon /> : <CheckBoxIcon />}
       </div>
       <div className="flex grow">
-        <div className="w-[86px] h-[86px] rounded-[10px] bg-gray_04">
-          <Image src="/img/image 68.png" alt="thumnail" width={62} height={62} className="p-3 box-content" />
+        <div className="w-[86px] md:w-[120px] h-[86px] md:h-[120px] rounded-[10px] bg-gray_04 p-3 box-content">
+          <Image
+            src="/img/image 68.png"
+            alt="thumnail"
+            width={62}
+            height={62}
+            className="w-full	h-full md:object-cover"
+          />
         </div>
         {/* todo: 상품 정보 props 내려주기 */}
         <CartItemInfo />
       </div>
       {/* todo: 삭제기능 연결 */}
-      <CloseIcon />
+      <div className="pr-[10px] md:ml-6 md:flex md:items-center">
+        <CloseIcon />
+      </div>
     </div>
   );
 }
@@ -34,14 +42,16 @@ function CartItemInfo({ name = '상품이름', price = 5000, discount = 500 }: C
   const originalPrice = price.toLocaleString('ko-KR');
   const discountedPrice = (price - discount).toLocaleString('ko-KR');
   return (
-    <div className="pl-3 flex flex-col justify-between">
-      <div className="flex flex-col">
+    <div className="pl-3 flex flex-col md:grow md:flex-row md:items-center justify-between md:justify-around">
+      <div className="flex flex-col md:grow md:flex-row md:items-center md:justify-between">
         {/* 상품명 */}
-        <h3 className="text-label-sm">{name}</h3>
-        {/* 할인적용 가격 */}
-        <span className="text-label-sm font-bold">{discountedPrice}원</span>
-        {/* 정가 */}
-        <span className="text-label-xs text-gray_01 line-through">{originalPrice}원</span>
+        <h3 className="text-label-sm md:text-[20px]">{name}</h3>
+        <div className="flex flex-col md:text-end">
+          {/* 할인적용 가격 */}
+          <span className="text-label-sm md:text-title-lg font-bold">{discountedPrice}원</span>
+          {/* 정가 */}
+          <span className="text-label-xs md:text-body-sm text-gray_01 line-through">{originalPrice}원</span>
+        </div>
       </div>
       {/* 최소수량 입력받기 */}
       <Counter min={10} />
@@ -55,7 +65,7 @@ type CounterPropsType = {
 function Counter({ min }: CounterPropsType) {
   const [quantity, setQuantity] = useState(min);
   return (
-    <div className="flex items-center justify-center  w-[78px] h-[26px] border-[1px] border-gray_02 rounded-[8px] box-content">
+    <div className="flex items-center justify-center  w-[78px] md:w-[112px] h-[26px] md:h-[36px] border-[1px] border-gray_02 rounded-[8px] box-content md:absolute">
       <div className="flex items-center justify-center px-2 py-1.5">
         {min !== quantity ? (
           <div onClick={() => setQuantity(quantity! - 1)}>
@@ -67,7 +77,7 @@ function Counter({ min }: CounterPropsType) {
           </div>
         )}
         <input
-          className="w-[34px] box-content text-label-sm text-center"
+          className="w-[34px] box-content text-label-sm md:text-title-md text-center"
           onChange={(e) => setQuantity(Number(e.target.value))}
           value={quantity}
           // 최소수량 보다 작게 입력 후 input창 블러시 최소수량으로 재지정
