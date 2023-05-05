99a71bc6dc96bb85bab81bd64ce9ff984f09572c
Hwa-J <hj1103.code@gmail.com>
2023-05-06 03:01:02 +0900

    feat: 장바구니 전체 선택과 장바구니 속 아이템 체크박스 연결 (미완성) - Issue #10

--- components/cart/CartItem.tsx
+++ components/cart/CartItem.tsx
@@ -1,20 +1,38 @@
 import Image from 'next/image';
 import { CheckBoxIcon, CheckedBoxIcon, CloseIcon } from '../Icons';
 import CartItemInfo from './CartItemInfo';
+import { useEffect, useState } from 'react';
 
 type CartItemPropsType = {
-  isSelected: boolean;
-  setIsSelected: React.Dispatch<React.SetStateAction<boolean>>;
+  isAllSelected: boolean;
+  setIsAllSelected: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-export default function CartItem({ isSelected, setIsSelected }: CartItemPropsType) {
+export default function CartItem({ isAllSelected, setIsAllSelected }: CartItemPropsType) {
+  const [isChecked, setIsChecked] = useState(isAllSelected);
+
+  // todo: 전체선택, 개별선택 기능 수정
+  useEffect(() => {
+    setIsChecked(isAllSelected);
+  }, [isAllSelected]);
+
+  // todo: 전체선택, 개별선택 기능 수정
+  const checkHandler = () => {
+    if (isChecked) {
+      setIsChecked(false);
+      setIsAllSelected(false);
+    } else {
+      setIsChecked(true);
+    }
+  };
+
   return (
     <div className="flex md:items-center md:w-[800px] py-[22px] border-b-[1px] border-b-gray_02">
-      <div className="pr-3 md:pl-7" onClick={() => setIsSelected(!isSelected)}>
-        {isSelected ? <CheckedBoxIcon /> : <CheckBoxIcon />}
+      <div className="pr-3 md:pl-7" onClick={checkHandler}>
+        {isChecked ? <CheckedBoxIcon /> : <CheckBoxIcon />}
       </div>
       <div className="flex grow">
-        <div className="w-[86px] md:w-[120px] h-[86px] md:h-[120px] rounded-[10px] bg-gray_04 p-3 box-content">
+        <div className="w-[86px] md:w-[120px] h-[86px] md:h-[120px] rounded-[10px] bg-gray_04 p-3 box-border">
           <Image
             src="/img/image 68.png"
             alt="thumbnail"
