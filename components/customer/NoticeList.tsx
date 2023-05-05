dbcd6711a1943a0c52bb0160ac77ec4cbb56b4d5
cheolgyun7 <https://github.com/cheolgyun7/React_Studying.git>
2023-05-05 20:51:57 +0900

    feat: 공지사항list 아코디언 적용및 정렬  #6

--- components/customer/NoticeList.tsx
+++ components/customer/NoticeList.tsx
@@ -20,19 +20,29 @@ export default function NoticeList({ item }: ItemListProps) {
 
     return (
         <div>
+            <div className="opacity-0 md:opacity-100 md:flex md:justify-between md:px-10 md:py-4 md:border-black md:border-y md:mt-4">
+                <span>번호</span>
+                <span>제목</span>
+                <span>작성일</span>
+            </div>
             {item?.map((item, index) => (
                 <div
-                    className="ml-3 pt-5 pb-2 border-b cursor-pointer relative"
+                    className="ml-3 pt-5 pb-5 border-b cursor-pointer relative md:ml-auto"
                     onClick={() => listClick(index)}
                     key={index}>
-                    <span className="block">{item.id}</span>
-                    <span className="text-sm">{item.title}</span>
+                    <span className="block md:inline-block md:ml-12">{item.id}</span>
+                    <span className="text-sm md:text-base md:ml-12">{item.title}</span>
                     {selectIndex === index &&
                         <>
                             <div
                                 className=" break-words py-9 px-5"
                             >
-                                {item.content}
+                                <h3 className="hidden md:block md:ml-8 md:mb-6">
+                                    - {item.title}
+                                </h3>
+                                <span className="md:ml-5">
+                                    {item.content}
+                                </span>
                             </div>
                         </>
                     }
