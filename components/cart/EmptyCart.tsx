7d28cffac0a27c8afa03c1da11a1aaa4ab52ad25
Hwa-J <hj1103.code@gmail.com>
2023-05-06 05:01:29 +0900

    refactor: Link 태그에 btn-mobile, btn-primary 유틸리티 클래스로 변경 적용 - Issue #10

--- components/cart/EmptyCart.tsx
+++ components/cart/EmptyCart.tsx
@@ -6,11 +6,9 @@ export default function EmptyCart() {
       <div className="w-full h-screen flex items-center justify-center m-auto text-gray_01">
         장바구니에 담긴 제품이 없습니다.
       </div>
-      <Link
-        href="/"
-        className="absolute bottom-[90px] flex items-center justify-center w-full rounded-[8px] bg-primary cursor-pointer"
-      >
-        <span className="py-[18px] text-white">구매하러 가기</span>
+      {/* 바로구매 페이지로 이동 */}
+      <Link href="/" className="btn-mobile btn-primary">
+        <span className="py-[18px]">구매하러 가기</span>
       </Link>
     </>
   );
