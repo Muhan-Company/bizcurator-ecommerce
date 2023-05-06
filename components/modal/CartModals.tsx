3002d6697d609665455e31cbf97cf34f72c81988
Hwa-J <hj1103.code@gmail.com>
2023-05-06 16:58:31 +0900

    feat: 장바구니 상품 삭제 완료 모달 UI 완성 - Issue #20

--- components/modal/CartModals.tsx
+++ components/modal/CartModals.tsx
@@ -1,7 +1,7 @@
 function CartItemDeleteModal() {
   return (
     <div className="absolute center inset-0 bg-black/20 z-20">
-      <div className="modal-shape flex flex-col gap-[26px] pt-[18px]">
+      <div className="modal-shape flex flex-col gap-[26px] pt-[18px] pb-[5px]">
         선택한 제품을 삭제하시겠습니까?
         <div className="center gap-2">
           <button className="btn-white w-[156px] h-[50px] py-[19px]">취소</button>
@@ -12,8 +12,22 @@ function CartItemDeleteModal() {
   );
 }
 
+function CartItemDeleteCompletedModal() {
+  return (
+    <div className="absolute center inset-0 bg-black/20 z-20">
+      <div className="modal-shape flex flex-col gap-[26px] pt-[18px] pb-[5px]">
+        삭제가 완료되었습니다.
+        <div className="center gap-2">
+          <button className="btn-white w-[156px] h-[50px] py-[19px]">닫기</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 const CartModals = {
   CartItemDeleteModal,
+  CartItemDeleteCompletedModal,
 };
 
 export default CartModals;
