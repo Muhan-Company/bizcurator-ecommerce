1f61773c112014f83503eb44573590e102e38b85
Hwa-J <hj1103.code@gmail.com>
2023-05-07 05:00:43 +0900

    feat: 마이페이지 메인 컴포넌트 UI 완성 - Issue #27

---
+++ pages/mypage/index.tsx
@@ -0,0 +1,9 @@
+import MyPageMain from '@/components/mypage/MyPageMain';
+
+export default function MyPage() {
+  return (
+    <div className="px-[22px]">
+      <MyPageMain />
+    </div>
+  );
+}
