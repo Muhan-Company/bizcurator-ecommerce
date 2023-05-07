1bf0186ea0ff640d813b7157221301b56719ccb7
Valentin1495 <newface95@icloud.com>
2023-05-07 21:53:02 +0900

    chore: Pull branch develop and merge

---
+++ components/body/PromoBanner.tsx
@@ -0,0 +1,3 @@
+export default function PromoBanner() {
+  return <div>PromoBanner</div>;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import MainBanner from '@/components/body/MainBanner';
+import PromoBanner from '@/components/body/PromoBanner';
 import ProductCategoryList from '@/components/body/ProductCategoryList';
 import Header from '@/components/header/Header';
 
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <MainBanner />
       <ProductCategoryList />
+      <PromoBanner />
     </main>
   );
 }
