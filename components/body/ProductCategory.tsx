66b42ac2df3f7b7faeef109036ec37437037c96b
Valentin1495 <newface95@icloud.com>
2023-05-08 18:49:16 +0900

    chore: 스타일 수정 - Issue #31

--- components/body/ProductCategory.tsx
+++ components/body/ProductCategory.tsx
@@ -8,7 +8,7 @@ interface CategoryType {
 
 export default function ProductCategory({ href, icon, name }: CategoryType) {
   return (
-    <Link href={href} className="flex flex-col items-center w-24">
+    <Link href={href} className="flex flex-col items-center w-20 sm:w-24">
       <div className="w-10 h-10 flex items-center justify-center">{icon}</div>
       <h3 className="font-normal text-button-xs">{name}</h3>
     </Link>
--- components/body/ProductCategoryList.tsx
+++ components/body/ProductCategoryList.tsx
@@ -15,8 +15,8 @@ import ProductCategory from './ProductCategory';
 export default function ProductCategoryList() {
   return (
     <div className="px-3 mt-10 space-y-6 block lg:hidden">
-      <h1 className="font-normal text-head-xs">전체 상품 보기</h1>
-      <div className="grid grid-cols-4 md:grid-cols-5 gap-3">
+      <h1 className="font-normal text-head-xs text-main">전체 상품 보기</h1>
+      <div className="grid grid-cols-4 md:grid-cols-5 gap-3 center">
         <ProductCategory href="#" icon={LampIcon({ width: '28', height: '28' })} name="객실용품" />
         <ProductCategory href="#" icon={ShowerIcon({ width: '30', height: '30' })} name="욕실용품" />
         <ProductCategory href="#" icon={ToothBrushIcon({ width: '24', height: '24' })} name="위생용품" />
