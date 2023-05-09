f53650d0808c685dbf01d397cc5e6eb2db9b5df1
Valentin1495 <newface95@icloud.com>
2023-05-09 17:23:48 +0900

    feat: 각 버튼 클릭 시 기능 구현 - Issue #35
    1. 메인은 홈페이지로 리다이렉트
    2. 검색은 모달 팝 업
    3. 마이페이지는 /mypage로 리다이렉트

--- components/body/NavBar.tsx
+++ components/body/NavBar.tsx
@@ -1,20 +1,35 @@
+import Link from 'next/link';
 import { HomeIcon, MagnifyingGlassIcon, UserIcon } from '../Icons';
+import { useRecoilState } from 'recoil';
+import { logInModalState, searchModalState } from '@/atoms/modalAtoms';
+import { createPortal } from 'react-dom';
+import SearchModal from '../header/SearchModal';
 
 export default function NavBar() {
+  const [showSearchModal, setShowSearchModal] = useRecoilState(searchModalState);
+
+  const openModal = () => {
+    setShowSearchModal(true);
+    document.body.classList.add('modal-open');
+  };
+
   return (
-    <div className="z-10 grid grid-cols-3 fixed left-0 bottom-0 right-0 h-16 bg-white rounded-t-[10px] px-3">
-      <button className="navbar-btn">
+    <div className="z-10 grid grid-cols-3 fixed left-0 bottom-0 right-0 h-16 bg-white rounded-t-[10px] px-3 shadow-[0_-4px_8px_rgba(0,0,0,0.08)]">
+      <Link href={'/'} className="navbar-btn">
         <HomeIcon />
         <span className="navbar-text">메인</span>
-      </button>
-      <button className="navbar-btn">
+      </Link>
+      <button className="navbar-btn" onClick={openModal}>
         <MagnifyingGlassIcon color="primary" />
         <span className="navbar-text">검색</span>
       </button>
-      <button className="navbar-btn">
+
+      {showSearchModal && createPortal(<SearchModal setShowSearchModal={setShowSearchModal} />, document.body)}
+
+      <Link href={'mypage'} className="navbar-btn">
         <UserIcon color="primary" />
         <span className="navbar-text">마이페이지</span>
-      </button>
+      </Link>
     </div>
   );
 }
--- components/header/TopHeader.tsx
+++ components/header/TopHeader.tsx
@@ -1,13 +1,19 @@
 import Link from 'next/link';
 import { Cart, LogoIcon, MagnifyingGlassIcon, UserIcon } from '../Icons';
-import { useState } from 'react';
+import { useRecoilState } from 'recoil';
 import { createPortal } from 'react-dom';
 import LogInModal from './LogInModal';
 import SearchModal from './SearchModal';
+import { logInModalState, searchModalState } from '@/atoms/modalAtoms';
 
 export default function TopHeader() {
-  const [showSearchModal, setShowSearchModal] = useState<boolean>(false);
-  const [showLogInModal, setShowLogInModal] = useState<boolean>(false);
+  const [showSearchModal, setShowSearchModal] = useRecoilState(searchModalState);
+  const [showLogInModal, setShowLogInModal] = useRecoilState(logInModalState);
+
+  const openModal = () => {
+    setShowSearchModal(true);
+    document.body.classList.add('modal-open');
+  };
 
   return (
     <div className="px-6 lg:px-0 flex justify-between items-center">
@@ -23,12 +29,12 @@ export default function TopHeader() {
           <Link href={'#'}>고객센터</Link>
         </div>
         <div className="flex items-center space-x-5 ml-auto">
-          <button className="header-btn" onClick={() => setShowSearchModal(true)}>
-            <MagnifyingGlassIcon />
+          <button className="header-btn" onClick={openModal}>
+            <MagnifyingGlassIcon color="main" />
           </button>
           {showSearchModal && createPortal(<SearchModal setShowSearchModal={setShowSearchModal} />, document.body)}
           <button className="hidden lg:inline header-btn">
-            <UserIcon />
+            <UserIcon color="main" />
           </button>
           <button className="header-btn">
             <Cart className="w-5 lg:w-8 h-5 lg:h-8" />
