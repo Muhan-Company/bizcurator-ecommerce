b6aa1c0d3642d7ff16870c97b5751dc077a33944
Valentin1495 <newface95@icloud.com>
2023-05-14 03:49:22 +0900

    feat: Notification 컴포넌트 - Issue #55

--- components/request/MyInfo.tsx
+++ components/request/MyInfo.tsx
@@ -1,12 +1,18 @@
+import { toast } from 'react-hot-toast';
+import Notification from '../Toasters/Notification';
+
 export default function MyInfo() {
+  const notify = () => toast('수정은 마이페이지에서 가능합니다.');
+
   return (
-    <div className="mt-10 px-3">
+    <div className="mt-10 mx-3" onClick={notify}>
       <h1 className="font-bold text-gray_01 text-body-md">구매 담당자 직통번호</h1>
       <h3 className="font-normal text-body-sm text-gray_01">구매 신청 및 담당 책임자의 직통 번호</h3>
       <section className="space-y-[10px] mt-4">
         <h3 className="my-info">이름</h3>
         <h3 className="my-info">번호</h3>
       </section>
+      <Notification />
     </div>
   );
 }
