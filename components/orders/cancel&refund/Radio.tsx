0888e3c1c5b6100314222468a0cee54872af3372
Hwa-J <hj1103.code@gmail.com>
2023-05-15 05:56:41 +0900

    feat: 라디오 컨테이너, 라디오 인풋  재사용 컴포넌트 생성 - Issue #48

---
+++ components/orders/cancel&refund/Radio.tsx
@@ -0,0 +1,47 @@
+import { FormEventHandler } from 'react';
+import SubTitleLayout from './SubtitleLayout';
+
+type RadioInputContainerProps = {
+  title: string;
+  children: React.ReactNode;
+  onChange: FormEventHandler;
+};
+function RadioInputContainer({ title, children, onChange }: RadioInputContainerProps) {
+  return (
+    <SubTitleLayout title={title}>
+      <div className="mb-10 flex text-gray_01 text-label-sm" onChange={onChange}>
+        {children}
+      </div>
+    </SubTitleLayout>
+  );
+}
+
+type RadioInputProps = {
+  name: string;
+  label: string;
+  defaultChecked?: boolean;
+};
+function RadioInput({ name, label, defaultChecked }: RadioInputProps) {
+  return (
+    <div className="flex items-center mr-9 hover:text-main">
+      <input
+        type="radio"
+        id={label}
+        name={name}
+        value={label}
+        defaultChecked={defaultChecked}
+        className={`peer/${label} radio-custom`}
+      />
+      <label htmlFor={label} className={`peer-checked/${label}:text-main`}>
+        {label}
+      </label>
+    </div>
+  );
+}
+
+const Radio = {
+  RadioInputContainer,
+  RadioInput,
+};
+
+export default Radio;
