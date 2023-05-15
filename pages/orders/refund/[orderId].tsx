84efe1ccae7775065be481794bf39f48ba2e9030
Hwa-J <hj1103.code@gmail.com>
2023-05-15 17:46:26 +0900

    feat: 환불 신청 페이지 생성 - Issue #48

---
+++ pages/orders/refund/[orderId].tsx
@@ -0,0 +1,24 @@
+import Layout from '@/components/layout/Layout';
+import NavBar from '@/components/footer/NavBar';
+import CancelRefundOrderInfo from '@/components/orders/cancel&refund/CancelRefundOrderInfo';
+import CancelRefundItemInfo from '@/components/orders/cancel&refund/CancelRefundItemInfo';
+import RefundRequestInfo from '@/components/orders/cancel&refund/RefundRequestInfo';
+
+export default function Refund() {
+  return (
+    <Layout>
+      <div className="mx-3 pb-[80px]">
+        <CancelRefundOrderInfo total_cost={2000000} delivery_state={2} />
+        <CancelRefundItemInfo
+          title="환불 제품 정보"
+          image={'/img/image 68.png'}
+          name="호텔용 타월"
+          quantity={2}
+          cost_per_one={1000000}
+        />
+        <RefundRequestInfo />
+      </div>
+      <NavBar />
+    </Layout>
+  );
+}
