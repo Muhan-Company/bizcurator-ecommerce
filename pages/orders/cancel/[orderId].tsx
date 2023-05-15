de5022fdfc720e0fa68113260e37f334072792e2
Hwa-J <hj1103.code@gmail.com>
2023-05-15 17:40:48 +0900

    refactor: 폴더 및 파일명 변경된 컴포넌트 수정 적용 - Issue #48 #47

--- pages/orders/cancel/[orderId].tsx
+++ pages/orders/cancel/[orderId].tsx
@@ -1,15 +1,20 @@
-import { useRouter } from 'next/router';
 import Layout from '@/components/layout/Layout';
 import NavBar from '@/components/footer/NavBar';
-import CancelOrderInfo from '@/components/orders/cancel/CancelOrderInfo';
-import CancelItemInfo from '@/components/orders/cancel/CancelItemInfo';
-import CancelInfo from '@/components/orders/cancel/CancelInfo';
+import CancelRefundOrderInfo from '@/components/orders/cancel&refund/CancelRefundOrderInfo';
+import CancelRefundItemInfo from '@/components/orders/cancel&refund/CancelRefundItemInfo';
+import CancelInfo from '@/components/orders/cancel&refund/CancelInfo';
 export default function Cancel() {
   return (
     <Layout>
       <div className="mx-3 pb-[80px]">
-        <CancelOrderInfo total_cost={2000000} delivery_state={0} />
-        <CancelItemInfo image={'/img/image 68.png'} name="호텔용 타월" quantity={2} cost_per_one={1000000} />
+        <CancelRefundOrderInfo total_cost={2000000} delivery_state={0} />
+        <CancelRefundItemInfo
+          title="주문취소 제품 정보"
+          image={'/img/image 68.png'}
+          name="호텔용 타월"
+          quantity={2}
+          cost_per_one={1000000}
+        />
         <CancelInfo />
       </div>
       <NavBar />
