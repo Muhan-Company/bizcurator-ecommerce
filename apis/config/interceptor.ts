7735e78b9659ed5950d7206dedf0356e21a0827d
Hwa-J <hj1103.code@gmail.com>
2023-05-16 18:07:12 +0900

    conf: Axios 라이브러리 설치 및 intance, interceptor(진행중) 설정 - Issue #59

---
+++ apis/config/interceptor.ts
@@ -0,0 +1,35 @@
+import axiosInstance from '.';
+
+// 요청 인터셉터 설정
+axiosInstance.interceptors.request.use(
+  (config) => {
+    // 요청 전에 수행할 작업을
+
+    // todo: 토큰 추가 interceptor 작성하기
+    // const token = /* 토큰 넣기 */;
+    // if (token) {
+    //   // 토큰이 존재하는 경우 헤더에 토큰 추가
+    //   config.headers['Authorization'] = `Bearer ${token}`;
+    // }
+    return config;
+  },
+  (error) => {
+    // 요청 에러 처리
+    return Promise.reject(error);
+  },
+);
+
+// 응답 인터셉터 설정
+axiosInstance.interceptors.response.use(
+  (response) => {
+    // 응답 데이터 전에 수행할 작업
+
+    return response;
+  },
+  (error) => {
+    // 응답 에러 처리
+    return Promise.reject(error);
+  },
+);
+
+export default axiosInstance;
