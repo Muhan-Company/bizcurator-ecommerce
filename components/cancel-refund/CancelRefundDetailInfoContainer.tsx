9d561016729ea2b50680d6cd86ca8eb15b5d292e
Hwa-J <hj1103.code@gmail.com>
2023-05-18 04:41:39 +0900

    feat: 주문취소, 환불 상세보기 페이지 UI 완성 - Issue #67

---
+++ components/cancel-refund/CancelRefundDetailInfoContainer.tsx
@@ -0,0 +1,21 @@
+import CancelRefundDetailOrderInfo from './CancelRefundDetailOrderInfo';
+import RefundInfo from './RefundInfo';
+import OrderDetailLayout from '../orders/[paymentId]/OrderDetailLayout';
+import { useRouter } from 'next/router';
+
+export default function CancelRefundDetailInfoContainer() {
+  const { pathname } = useRouter();
+  const title = pathname.includes('cancellations') ? '주문취소' : '환불';
+  return (
+    <div>
+      {
+        /* 상태값 반려일 경우 랜더링*/
+        <OrderDetailLayout.OrderDetailTextContentLayout title={`${title} 반려 사유`}>
+          <p className="text-body-xs">{'이미 배송이 진행되었기 때문에 주문취소가 어렵습니다.'}</p>
+        </OrderDetailLayout.OrderDetailTextContentLayout>
+      }
+      {/* 상태값 승인일 경우 랜더링*/ <RefundInfo />}
+      <CancelRefundDetailOrderInfo />
+    </div>
+  );
+}
