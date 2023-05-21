e9041a5894b0ff61e0b262235bb5c652ce5dfcbe
Hwa-J <hj1103.code@gmail.com>
2023-05-22 01:51:57 +0900

    refactor: 사용하지 않는 상수 삭제 - Issue #71

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,14 +1,1 @@
 export const REFRESH_URL = `${process.env.NEXT_PUBLIC_API_URL}/api/users/refresh`;
-
-export const REG_EXP = {
-  SIGN_UP: {
-    EMAIL: new RegExp('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'),
-    PW: new RegExp('^[a-zA-Z0-9]{8,16}$'),
-  },
-};
-
-export const ERROR_MESSAGES = {
-  SIGN_UP: {
-    ID: '아이디(e-mail)형식에 맞지 않습니다.',
-  },
-};
