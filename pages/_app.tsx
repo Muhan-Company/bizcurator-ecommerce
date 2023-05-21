da18e97a3ab6a76f14f2520e19fefeb5c54ec49b
cheolgyun7 <https://github.com/cheolgyun7/React_Studying.git>
2023-05-22 02:13:40 +0900

    App.tsx충돌해결

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RecoilRoot } from 'recoil';
-import Notification from '@/components/toasters/Notification';
+import Notification from '@/components/Toasters/Notification';
 
 const queryClient = new QueryClient();
 
@@ -10,9 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
-
         <Component {...pageProps} />
-
         <Notification />
       </RecoilRoot>
     </QueryClientProvider>
