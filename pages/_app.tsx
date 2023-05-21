9f937a44e40b8a4d421eda132c0fdad3685b01bf
Hwa-J <hj1103.code@gmail.com>
2023-05-22 02:04:33 +0900

    refactor: 충돌 해결 - Issue #71

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RecoilRoot } from 'recoil';
 import Notification from '@/components/Toasters/Notification';
+import ScrollToTop from '@/components/ScrollToTop';
 
 const queryClient = new QueryClient();
 
