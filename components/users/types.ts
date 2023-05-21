7ed83ac85036cf5fe28e2c679dab38a15742308c
Hwa-J <hj1103.code@gmail.com>
2023-05-22 01:34:53 +0900

    refactor: 회원가입폼 type 추가 - Issue #71

--- components/users/types.ts
+++ components/users/types.ts
@@ -2,3 +2,22 @@ export interface LoginFormValues {
   username: string;
   password: string;
 }
+interface IndexingTypes {
+  [key: string]: string | undefined;
+}
+export interface SignupFormValues extends IndexingTypes {
+  username: string;
+  password: string;
+  password_confirm: string;
+  business_name: string;
+  representative: string;
+  business_number: string;
+  postal_code?: string;
+  postalCode?: string;
+  address: string;
+  detailAddress?: string;
+  business_registration_Image: string;
+  manager: string;
+  manager_email: string;
+  manager_phone_number: string;
+}
