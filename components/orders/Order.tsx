f0068752553ecc2408e8224ff6b5378175d1a712
Hwa-J <hj1103.code@gmail.com>
2023-05-22 09:22:01 +0900

    refactor: 주문 리스트, 주문 상품 등 API 연결을 위한 수정 - Issue #32 #43

--- components/orders/Order.tsx
+++ components/orders/Order.tsx
@@ -1,27 +1,29 @@
 import Link from 'next/link';
 import { ChevronRightIcon } from '../Icons';
-import OrderItem from './OrderItem';
+import OrderItem, { ItemData } from './OrderItem';
 
-type OrderProps = {
+export type OrderProps = {
   orderTime?: string;
   paymentId?: string;
+  orderInfo: ItemData[];
 };
 
-export default function Order({ orderTime = '2023-04-20', paymentId = '123456546864' }: OrderProps) {
+export default function Order({ orderTime = '2023-04-20', paymentId = '123456546864', orderInfo }: OrderProps) {
   return (
     <section className="pt-[22px] pb-[18px] my-2">
       <div className="pb-[14px] flex flex-col border-b-[1px] border-b-black">
         <div className="pb-3 flex items-center justify-between font-medium">
           <div>{orderTime}</div>
-          <Link href={`/user/orders/${paymentId}`} className="center text-label-sm text-gray_01">
+          <Link href={`/orders/detail/${paymentId}`} className="center text-label-sm text-gray_01">
             전체주문 상세보기 <ChevronRightIcon color="#999" />
           </Link>
         </div>
         <div className="text-label-sm text-gray_01">주문번호 : {paymentId}</div>
       </div>
-      {/* todo: api 데이터 받아 map함수 사용 */}
-      {/* todo: image, name, amount, delivery_state props 내려주기*/}
-      <OrderItem />
+
+      {orderInfo?.map((item) => (
+        <OrderItem key={item?.orderId} item={item} />
+      ))}
     </section>
   );
 }
