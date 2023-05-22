eed65e3f342af40ab1b47aa665edd46edf1aeccd
Hwa-J <hj1103.code@gmail.com>
2023-05-22 09:17:20 +0900

    feat: 주문상세 컨테이너 컴포넌트 생성 및 API 데이터 연결을 위한 수정 - Issue #43

--- components/orders/[paymentId]/OrderInfo.tsx
+++ components/orders/[paymentId]/OrderInfo.tsx
@@ -9,7 +9,7 @@ export default function OrderInfo({ payment_id, total_cost }: OrderInfoProps) {
   return (
     <OrderDetailLayout.OrderDetailTextContentLayout title="주문 정보">
       <OrderDetailLayout.OrderDetailTextContent title="주문번호" value={payment_id} className="font-medium" />
-      <OrderDetailLayout.OrderDetailTextContent title="결제금액" value={`${total_cost.toLocaleString('kr-KR')}원`} />
+      <OrderDetailLayout.OrderDetailTextContent title="결제금액" value={total_cost?.toLocaleString('kr-KR') + '원'} />
     </OrderDetailLayout.OrderDetailTextContentLayout>
   );
 }
