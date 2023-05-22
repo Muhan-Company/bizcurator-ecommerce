d67d1d67ed400387cca9df972e3237d6d8a6afc7
Hwa-J <hj1103.code@gmail.com>
2023-05-22 09:09:02 +0900

    refactor: 주문내역 빈 페이지 조회 내역 이름 동적 데이터로 수정 - Issue #32

--- components/orders/EmptyOrderList.tsx
+++ components/orders/EmptyOrderList.tsx
@@ -1,3 +1,14 @@
+import { useRouter } from 'next/router';
+
+const DELEVERY_STATE: { [key: string]: string } = {
+  paid: '결제완료',
+  delivering: '배송중',
+  deliver_done: '배송완료',
+  finish: '구매확정',
+};
+
 export default function EmptyOrderList() {
-  return <div className="absolute inset-0 w-full h-screen z-[-1] center text-gray_01 ">결제완료 내역이 없습니다.</div>;
+  const { query } = useRouter();
+  const currentPage = query?.state as string;
+  return <div className="w-full h-[50vh] center text-gray_01">{DELEVERY_STATE[currentPage]} 내역이 없습니다.</div>;
 }
