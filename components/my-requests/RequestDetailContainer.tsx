8b227059b65feb82d55c00083c9ff01043808689
Valentin1495 <newface95@icloud.com>
2023-05-26 17:32:05 +0900

    fix: props 추가

--- components/my-requests/RequestDetailContainer.tsx
+++ components/my-requests/RequestDetailContainer.tsx
@@ -2,18 +2,21 @@ import RequestDetailHeader from '@/components/my-requests/RequestDetailHeader';
 import PurchaseForm from '@/components/request/PurchaseForm';
 import ManufactureForm from '../request/ManufactureForm';
 import { useRouter } from 'next/router';
+import { useGetMyInfo } from '@/apis/users';
 
 export default function RequestDetailContainer() {
   const { pathname } = useRouter();
+  const info = useGetMyInfo();
+
   return (
     <div>
       <RequestDetailHeader state={'반려'} />
       <div className="opacity-50 cursor-not-allowed	">
         {/* todo: 페이지 경로에 따라 다른 폼 보여주기 */}
         {pathname.includes('purchase') ? (
-          <PurchaseForm />
+          <PurchaseForm {...info} />
         ) : pathname.includes('manufacture') ? (
-          <ManufactureForm />
+          <ManufactureForm {...info} />
         ) : (
           <div>판매의뢰 폼</div>
         )}
