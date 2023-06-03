9711dc67ba5e0a3ee17a11d209c51044db526507
Valentin1495 <newface95@icloud.com>
2023-06-03 18:08:21 +0900

    fix: 빌드 실패 에러 해결

--- components/cart/CartItemList.tsx
+++ components/cart/CartItemList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import CartItem from './CartItem';
 import EmptyCart from './EmptyCart';
 import Link from 'next/link';
@@ -105,69 +105,47 @@ export default function CartItemList() {
 
   if (isLoading) return <Loader className="h-[494px]" />;
   if (isError) return <p className="text-red center h-[494px] font-bold text-xl">장바구니 조회 실패</p>;
-  if (isSuccess)
-    return (
-      <div className="relative h-[494px]">
-        {cartItems.length === 0 ? (
-          <EmptyCart />
-        ) : (
-          <div className="pt-5 pb-20 mx-3">
-            <div className="flex items-center mb-[22px] pb-3 border-b-[1px] border-black">
-              <SelectAllCheckbox
-                total={cartItems.length}
-                selected={selectedItems.length}
-                selectAll={selectAll}
-                handleSelectAll={handleSelectAll}
-              />
-              {/* todo: 삭제 모달 연결 */}
-              <button disabled={!selectedItems.length} onClick={showModal} className="ml-4 font-medium disabled-btn">
-                선택삭제
-              </button>
-            </div>
-            {cartItems.map((item) => (
-              <CartItem key={item.product_id} item={item} toggleItem={toggleItem} handleQtyChange={handleQtyChange} />
-            ))}
 
-            <CartPaymentAmountInfo totalCost={totalPrice} totalDiscount={totalDiscountPrice} />
-
-            <div className="center gap-[7px] mt-[76px]">
-              {/* todo: 바로구매 페이지로 이동 */}
-              <Link href="/products/categories/0?sort=newest" className="w-[172px] h-[50px] btn-white grow py-[19px]">
-                다른 제품 보기
-              </Link>
-              {/* todo: 결제 페이지로 이동 */}
-              <button onClick={openModal} className="w-[172px] h-[50px] btn-primary grow py-[19px]">
-                구매하기
-              </button>
-            </div>
+  return (
+    <div className="relative h-[494px]">
+      {cartItems?.length === 0 ? (
+        <EmptyCart />
+      ) : (
+        <div className="pt-5 pb-20 mx-3">
+          <div className="flex items-center mb-[22px] pb-3 border-b-[1px] border-black">
+            <SelectAllCheckbox
+              total={cartItems?.length}
+              selected={selectedItems.length}
+              selectAll={selectAll}
+              handleSelectAll={handleSelectAll}
+            />
+            {/* todo: 삭제 모달 연결 */}
+            <button disabled={!selectedItems.length} onClick={showModal} className="ml-4 font-medium disabled-btn">
+              선택삭제
+            </button>
           </div>
-        )}
-        {showRemoveItemModal &&
-          createPortal(<CartDeleteModals.CartItemDeleteModal selectedItems={selectedItems} />, document.body)}
-        {showRemoveCompleteModal && createPortal(<CartDeleteModals.DeleteCompletedModal />, document.body)}
-        {showBuyCompleteModal && createPortal(<BuyCompleteModal />, document.body)}
-      </div>
-    );
+          {cartItems?.map((item) => (
+            <CartItem key={item.product_id} item={item} toggleItem={toggleItem} handleQtyChange={handleQtyChange} />
+          ))}
+
+          <CartPaymentAmountInfo totalCost={totalPrice} totalDiscount={totalDiscountPrice} />
+
+          <div className="center gap-[7px] mt-[76px]">
+            {/* todo: 바로구매 페이지로 이동 */}
+            <Link href="/products/categories/0?sort=newest" className="w-[172px] h-[50px] btn-white grow py-[19px]">
+              다른 제품 보기
+            </Link>
+            {/* todo: 결제 페이지로 이동 */}
+            <button onClick={openModal} className="w-[172px] h-[50px] btn-primary grow py-[19px]">
+              구매하기
+            </button>
+          </div>
+        </div>
+      )}
+      {showRemoveItemModal &&
+        createPortal(<CartDeleteModals.CartItemDeleteModal selectedItems={selectedItems} />, document.body)}
+      {showRemoveCompleteModal && createPortal(<CartDeleteModals.DeleteCompletedModal />, document.body)}
+      {showBuyCompleteModal && createPortal(<BuyCompleteModal />, document.body)}
+    </div>
+  );
 }
-
-{
-  /* <CartPaymentAmountInfo
-  totalCost={data[0]?.regularPrice}
-  totalCost={getTotalCost(cartItems)}
-  totalDiscount={data[0]?.regularPrice - data[0]?.discountPrice}
-  totalDiscount={getDiscountCost(cartItems)}
-/> */
-}
-
-// const getTotalCost = (data: { regular_price: number }[]) => {
-//   let totalCost = 0;
-//   data?.map((cartItem) => (totalCost += cartItem.regular_price));
-
-//   return totalCost;
-// };
-// const getDiscountCost = (data: { regular_price: number; discount_price: number }[]) => {
-//   let totalDiscountCost = 0;
-//   data?.map((cartItem) => (totalDiscountCost += cartItem.regular_price - cartItem.discount_price));
-
-//   return totalDiscountCost;
-// };
